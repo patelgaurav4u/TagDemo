fcce78f0d90051ec3f26141e3023c2b324f23fcb
Gaurav <gaurav.patel@kukd.com>
2015-11-25 13:42:26 +0530

    firstComit

---
+++ TagDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TagDemo
+//
+//  Created by Asquare on 25/11/15.
+//  Copyright © 2015 Asquare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TagDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TagDemo
+//
+//  Created by Asquare on 25/11/15.
+//  Copyright © 2015 Asquare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
